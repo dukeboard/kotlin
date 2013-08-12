1216a82cab294a7ee6ff3dd4055743b4ce4d75a7
Svetlana Isakova <svetlana.isakova@jetbrains.com>
2013-08-12 12:52:08 +0400

    ++added test for nontrivial nested calls

--- compiler/testData/diagnostics/tests/inference/nestedCalls/nontrivialCallExpression.kt
+++ compiler/testData/diagnostics/tests/inference/nestedCalls/nontrivialCallExpression.kt
@@ -4,7 +4,7 @@ fun foo(l: List<Int>): Int = l.get(0)
 
 fun <T> emptyList(): List<T> = throw Exception()
 
-fun <T: Any> makeNullable(t: T): T? = null
+fun <T: Any> makeNullable(<!UNUSED_PARAMETER!>t<!>: T): T? = null
 
 fun bar(i: Int) = i
 fun bar(a: Any) = a
